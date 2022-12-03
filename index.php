714c4eecef76e8c66095744aed2c1eacc1cd5063
daniel tocino <daniel@lookingformypassion.com>
2022-12-03 17:52:00 +0800

    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>default</title>
+    </head>
+    <body>
+        <div class="navbarDiv">
+            <a class="navLink" href="">Home</a>
+            <a class="navLink" href="">News</a>
+            <a class="navLink" href="">Contact</a>
+            <a class="navLink" href="">About</a>
+        </div>
+    </body>
+    <?php ?>
+</html>
